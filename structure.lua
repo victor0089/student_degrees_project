e7d280c2f2e052397ea2bd2a79a06a280dac6242
Victor Adly <42696918+victor0089@users.noreply.github.com>
2024-03-16 22:20:25 +0200

    Create structure.lua

---
+++ structure.lua
@@ -0,0 +1,22 @@
+student_degrees_project/
+│
+├── student_degrees_project/
+│   ├── __init__.py
+│   ├── settings.py
+│   ├── urls.py
+│   └── wsgi.py
+│
+├── student_degrees/
+│   ├── migrations/
+│   ├── templates/
+│   │   └── student_degrees/
+│   │       ├── student_list.html
+│   │       └── degree_list.html
+│   ├── __init__.py
+│   ├── admin.py
+│   ├── apps.py
+│   ├── models.py
+│   ├── tests.py
+│   └── views.py
+│
+└── manage.py
